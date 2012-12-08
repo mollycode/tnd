ec6199d1518c9eae5ac44626697985d8442c2eda
Andrew Choi <andrew@choi.com>
2012-12-08 15:26:04 -0500

    Improve login functionality

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
 
+    url(r'^login/$', 'users.views.login'),
     url(r'^profile/$', 'users.views.profile'),
     url(r'^register/$', 'users.views.register'),
 )
--- users/views.py
+++ users/views.py
@@ -1,14 +1,20 @@
 # Create your views here.
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+def login(request):
+    if request.POST:
+        return profile(request)
+    else:
+        return render(request, "login.html")
+
 def profile(request):
     if request.POST:
-        if request.POST['password1']:
+        if 'password1' in request.POST:
             password = request.POST['password1']
         else:
             password = request.POST['password']
@@ -17,8 +23,8 @@ def profile(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             if user.is_active:
-                login(request, user)
-                return render(request, "profile.html")
+                auth_login(request, user)
+                return render(request, "profile.html", {"username": username})
             else:
                 print "not an active user"
                 return render(request, "login.html")
