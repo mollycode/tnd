435b009eb4c0d7f0f1dcf79e5209fad7e52761e6
Andrew Choi <andrew@choi.com>
2012-12-26 22:40:22 -0800

    Update course models to be more accurate

---
+++ courses/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from courses.models import Course, CourseNight, Clip
+
+admin.site.register(Course)
+admin.site.register(CourseNight)
+admin.site.register(Clip)
--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Course(models.Model):
+    professor = models.ForeignKey(User)
+    title = models.CharField(max_length = 200)
+    description = models.CharField(max_length = 1000)
+    
+    def __unicode__(self):
+        return self.title
+    
+class CourseNight(models.Model):
+    course = models.ForeignKey(Course)
+    night = models.IntegerField()
+    
+    def __unicode__(self):
+        return str(self.course) + ": " + str(self.night)
+
+class Clip(models.Model):
+    course_night = models.ForeignKey(CourseNight)
+    youtube_video = models.URLField()
+    clip_num = models.IntegerField()
+    
+    """
+    def __unicode__(self):
+        return course_night.course + ": " + course_night.night + " - clip " + self.clip_num
+        """
