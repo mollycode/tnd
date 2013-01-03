07c6a7a5e7028d35054f488a4bdd3b4ab9878853
bko <bko@princeton.edu>
2013-01-03 11:29:02 -0500

    Add rotating "hot courses" in index

--- static/javascript.js
+++ static/javascript.js
@@ -8,67 +8,20 @@ $(document).ready(function() {
 			}*/
 		}); 
 		$("ul.thumbnails").ytplaylist({addThumbs:true, autoPlay: false, holderId: 'ytvideo1'});
-    } 
-); 
+		
 
-
-$.tablesorter.addParser({
-  id: 'monthDayYear',
-  is: function(s) {
-      return false;
-  },
-  format: function(s) {
-      var date = s.match(/^(\w{3})[ ](\d{1,2}),[ ](\d{4})$/);
-      var m = monthNames[date[1]];
-      var d = String(date[2]);
-      if (d.length == 1) {d = "0" + d;}
-      var y = date[3];
-	 
-      return '' + y + m + d;
-  },
-  type: 'numeric'
-});
-var monthNames = {};
-monthNames["Jan"] = "01";
-monthNames["Feb"] = "02";
-monthNames["Mar"] = "03";
-monthNames["Apr"] = "04";
-monthNames["May"] = "05";
-monthNames["Jun"] = "06";
-monthNames["Jul"] = "07";
-monthNames["Aug"] = "08";
-monthNames["Sept"] = "09";
-monthNames["Oct"] = "10";
-monthNames["Nov"] = "11";
-monthNames["Dec"] = "12";
-
-/*$(document).ready(function() {
-	$(".navtitle").mouseover(function() {
-		var img = $(this).find('img');
-		var file = img.attr('src').split('.')[0];
-		img.attr('src', file + 'hover.png');
-	}).mouseout(function() {
-		var img = $(this).find('img');
-		var file = img.attr('src').split('hover')[0];
-		img.attr('src', file + '.png');
-	});
+		var divs = $('div[id^="slideshow"]').hide(),
+		i = 0;
 	
-	
-	
-
-	$(".more").one( "click", function () {
-
-		$("#coursedescription").css( "height","+=200" );
-		$("#coursecontent").css("height", "+=200");
-		$(".author").css("top","-=200");
-		$("#courseright").css("top","-=200");
-
-	});
+		(function cycle() { 
+			divs.eq(i).effect("slide")
+					  .delay(8000)
+					  .show(0)
+					  .hide(0, cycle)
+					  
+			i = ++i % divs.length; // increment i, 
+								   //   and reset to 0 when it equals divs.length
+		})();
 
 
-
-
-
-});
-
-*/
+}); 
