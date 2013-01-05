fb3842edf25f529a14c4f2deeef1efc695d4a6d3
Andrew Choi <andrew@choi.com>
2013-01-05 00:11:18 -0500

    Add support for removing courses

--- users/urls.py
+++ users/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('users.views',
     url(r'^completedcourses/$', 'completedcourses'),
     
     url(r'^addcourse/(?P<course_id>\d+)/$', 'addcourse'),
+    url(r'^removecourse/(?P<course_id>\d+)/$', 'removecourse'),
     url(r'^finishcourse/(?P<course_id>\d+)/$', 'finishcourse'),
     
     url(r'^logout/$', 'logout'),
